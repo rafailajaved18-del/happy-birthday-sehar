c16b16c14c220a291db68753fb77647168260269
rafailajaved18-del <rafailajaved18@gmail.com>
2026-08-01 19:18:20 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("🎉 Happy Birthday Sehar! 🎂💖");
+
+document.addEventListener("click", function () {
+    document.body.style.background =
+        "linear-gradient(135deg,#ffd1e8,#ffeaf5,#ffffff)";
+});
