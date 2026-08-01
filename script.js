ab0af6e50a639372a8b6b0cd771f803e1644dda4
rafailajaved18-del <rafailajaved18@gmail.com>
2026-08-01 21:52:08 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 function openGift(){
-    let music = document.getElementById("music");
+
+let music = document.getElementById("music");
 music.play();
-   
-    document.querySelector(".loading").style.display="none";
-    document.getElementById("main").style.display="block";
 
-    for(let i=0;i<80;i++){
-        let heart=document.createElement("div");
-        heart.innerHTML="💖";
-        heart.style.position="fixed";
-        heart.style.left=Math.random()*100+"%";
-        heart.style.top="100%";
-        heart.style.fontSize=(20+Math.random()*25)+"px";
-        heart.style.pointerEvents="none";
-        heart.style.transition="all 6s linear";
+document.querySelector(".loading").style.display="none";
+document.getElementById("main").style.display="block";
 
-        document.body.appendChild(heart);
+for(let i=0;i<80;i++){
+    let heart=document.createElement("div");
+    heart.innerHTML="💖";
+    heart.style.position="fixed";
+    heart.style.left=Math.random()*100+"%";
+    heart.style.top="100%";
+    heart.style.fontSize=(20+Math.random()*25)+"px";
+    heart.style.pointerEvents="none";
+    heart.style.transition="all 6s linear";
 
-        setTimeout(()=>{
-            heart.style.top="-10%";
-        },100);
+    document.body.appendChild(heart);
 
-        setTimeout(()=>{
-            heart.remove();
-        },6000);
-    }
+    setTimeout(()=>{
+        heart.style.top="-10%";
+    },100);
+
+    setTimeout(()=>{
+        heart.remove();
+    },6000);
 }
+
+} 
