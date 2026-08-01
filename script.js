eb78fc83234e3f25f5787670ff8f89e1bfc6aa74
rafailajaved18-del <rafailajaved18@gmail.com>
2026-08-01 19:40:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-alert("🎉 Happy Birthday Sehar! 🎂💖");
+function openGift(){
+    document.querySelector(".loading").style.display="none";
+    document.getElementById("main").style.display="block";
 
-document.addEventListener("click", function () {
-    document.body.style.background =
-        "linear-gradient(135deg,#ffd1e8,#ffeaf5,#ffffff)";
-});
+    for(let i=0;i<80;i++){
+        let heart=document.createElement("div");
+        heart.innerHTML="💖";
+        heart.style.position="fixed";
+        heart.style.left=Math.random()*100+"%";
+        heart.style.top="100%";
+        heart.style.fontSize=(20+Math.random()*25)+"px";
+        heart.style.pointerEvents="none";
+        heart.style.transition="all 6s linear";
+
+        document.body.appendChild(heart);
+
+        setTimeout(()=>{
+            heart.style.top="-10%";
+        },100);
+
+        setTimeout(()=>{
+            heart.remove();
+        },6000);
+    }
+}
